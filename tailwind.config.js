4217991a3ddc56d8adc89ef24619baac311317f4
Fernando Lopes <118869201+plasmus777@users.noreply.github.com>
2024-10-09 22:15:29 -0300

    Upload dos Arquivos Base do Projeto

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,ts,jsx,tsx,html}", "./src/*.{js,ts,jsx,tsx,html}", "./*.{js,ts,jsx,tsx,html}", "./index.html",],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
